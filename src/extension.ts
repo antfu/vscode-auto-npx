873e195e4781ad48460cc6594f5eb75da90184d8
markthree <1801982702@qq.com>
2022-03-11 19:12:03 +0800

    style: format code style

--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,25 @@ import { window, workspace } from 'vscode'
 const isWindows = platform() === 'win32'
 
 const COMMAND = isWindows
-	? `set Path=%Path%;node_modules\\.bin`
-	: 'export PATH=$PWD/node_modules/.bin:$PATH'
+  ? 'set Path=%Path%;node_modules\\.bin'
+  : 'export PATH=$PWD/node_modules/.bin:$PATH'
 
 function setup() {
-	window.terminals.forEach(async t => {
-		if (await t.processId) return
-		t.sendText(COMMAND)
-	})
-	window.onDidOpenTerminal(t => {
-		t.sendText(COMMAND)
-	})
+  window.terminals.forEach(async(t) => {
+    if (await t.processId)
+      return
+    t.sendText(COMMAND)
+  })
+  window.onDidOpenTerminal((t) => {
+    t.sendText(COMMAND)
+  })
 }
 
 export async function activate() {
-	if (workspace.isTrusted) setup()
-	else workspace.onDidGrantWorkspaceTrust(setup)
+  if (workspace.isTrusted)
+    setup()
+  else
+    workspace.onDidGrantWorkspaceTrust(setup)
 }
 
 export function deactivate() {}
