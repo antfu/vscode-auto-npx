023a3e1777d0dc4ea52839bd610ef6a9566e373c
markthree <1801982702@qq.com>
2022-03-11 19:02:34 +0800

    feat: add windows support

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,25 @@
+import { platform } from 'os'
 import { window, workspace } from 'vscode'
 
-const COMMAND = 'export PATH=$PWD/node_modules/.bin:$PATH'
+const isWindows = platform() === 'win32'
+
+const COMMAND = isWindows
+	? `set Path=%Path%;node_modules\\.bin`
+	: 'export PATH=$PWD/node_modules/.bin:$PATH'
 
 function setup() {
-  window.terminals.forEach(async(t) => {
-    if (await t.processId)
-      return
-    t.sendText(COMMAND)
-  })
-  window.onDidOpenTerminal((t) => {
-    t.sendText(COMMAND)
-  })
+	window.terminals.forEach(async t => {
+		if (await t.processId) return
+		t.sendText(COMMAND)
+	})
+	window.onDidOpenTerminal(t => {
+		t.sendText(COMMAND)
+	})
 }
 
 export async function activate() {
-  if (workspace.isTrusted)
-    setup()
-  else
-    workspace.onDidGrantWorkspaceTrust(setup)
+	if (workspace.isTrusted) setup()
+	else workspace.onDidGrantWorkspaceTrust(setup)
 }
 
 export function deactivate() {}
